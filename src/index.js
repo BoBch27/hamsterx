5512fc16f0e95d4ae029a3d5fbe9f4f1481945ea
Bobby Donev <donev.bobby@gmail.com>
2025-10-15 00:51:56 +0100

    feat: add option to disable auto init

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import * as signals from "./signal.js";
 import * as directives from "./directives.js";
 
+// Check if auto-init should be disabled
+// User can set window.HamsterjsAutoInit = false before script loads
+const shouldAutoInit = typeof window !== 'undefined' && window.HamsterjsAutoInit !== false;
+
 // Auto-init directives when DOM ready (only for browser global usage)
-if (typeof document !== 'undefined') {
+if (typeof document !== 'undefined' && shouldAutoInit) {
     const autoInit = () => {
         if (document.body) {
             console.log('🐹 Hamster.js auto-initialised');
